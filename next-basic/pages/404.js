faabb301d42a438e4dc032ac49a83c2ea8f9c315
Zhaoyu(Henry) Zhong <zhaoyuzhong2015@gmail.com>
2022-09-09 16:27:29 +1000

    finished#9

--- next-basic/pages/404.js
+++ next-basic/pages/404.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
-
+import { useEffect } from "react";
+import Router, { useRouter } from "next/router";
 const NotFound = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      Router.push("/");
+    }, 3000);
+  }, []);
   return (
     <div className="not-found">
       <h1>Ooo ps...</h1>
       <h2>Page is not found</h2>
-
       <p>
         Back to the
         <Link href="/">
--- next-basic/pages/index.js
+++ next-basic/pages/index.js
@@ -1,17 +1,16 @@
-import Footer from "../components/Footer";
-import NavBar from "../components/navbar";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Home Page</title>
+        <meta name="keywords" content="whatever keyword in here" />
+      </Head>
       <h1 className={styles.title}>Home page</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore cupiditate nisi obcaecati
-        ratione laudantium consequuntur dolores, itaque fugit quae harum, debitis voluptas vel. Ab
-        non sit mollitia repellendus eveniet vel.
-      </p>
+      <p className={styles.text}>This is the place where you can show on the home page. </p>
       <Link href={"./ninjas"}>
         <a className={styles.btn}>See Ninjas</a>
       </Link>
