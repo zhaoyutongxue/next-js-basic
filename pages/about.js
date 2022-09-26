3932edb849274d8ed9b3be5f5c3d58782655892b
Zhaoyu(Henry) Zhong <zhaoyuzhong2015@gmail.com>
2022-09-26 22:16:28 +1000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -6,11 +6,7 @@ const about = () => {
         <title>Instant Noodle | about page</title>
       </Head>
       <h1>about page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, doloribus illum distinctio
-        at optio necessitatibus sunt consectetur architecto odio maiores laboriosam, reiciendis
-        ducimus atque. Saepe eos ut quibusdam deserunt debitis.
-      </p>
+      <p>here I will put something about myself</p>
     </div>
   );
 };
